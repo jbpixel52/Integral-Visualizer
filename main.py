abab2671e32fc4d7a0fe7d17cb7119df77fa5b38
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2020-11-05 16:26:32 -0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,36 @@
+
+from flask import Flask,redirect,url_for,render_template,request
+from sympy.parsing.sympy_parser import parse_expr
+
+
+def parse(expresion):
+    return parse_expr(expresion)
+
+
+app= Flask(__name__)
+
+@app.route("/")
+def index(name=None, methods=["GET","POST"]):
+    return render_template("index.html",name=name)
+
+@app.route("/plot")
+def graphic():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+
+
+parse('x**2')
+
+
+
+
+if __name__== "__main__":
+    app.run(debug=True,use_reloader=False)
