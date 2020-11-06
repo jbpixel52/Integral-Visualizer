c1e110361cffcbaec7f90e4150f2a428e9be4ef8
Omar Duran <54329113+OktaOEDC@users.noreply.github.com>
2020-11-05 19:17:08 -0800

    Ejemplo de figura de matplotlib
    Dos funciones, una hace la figura en binario y otra la genera en python, es un png en su propia direccion en el sitio

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-
-from flask import Flask,redirect,url_for,render_template,request
+import io
+import random
+from flask import Flask,redirect,url_for,render_template,request, Response
 from sympy.parsing.sympy_parser import parse_expr
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 
 def parse(expresion):
@@ -25,12 +28,22 @@ def graphic():
     # the code below is executed if the request method
     # was GET or the credentials were invalid
     return render_template('login.html', error=error)
-
-
 parse('x**2')
 
+@app.route("/experiment_matplotlib.png")
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
-
+def create_figure():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+    axis.plot(xs, ys)
+    return fig
 
 if __name__== "__main__":
     app.run(debug=True,use_reloader=False)
