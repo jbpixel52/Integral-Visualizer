7ff4b8cf48310659c44ecc67e190b7536431123e
Omar Duran <54329113+OktaOEDC@users.noreply.github.com>
2020-11-17 17:16:43 -0800

    Init
    Init con las librerias escenciales

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # %%
-from flask import Flask, redirect, url_for, render_template, request
+import io
+import random
+from flask import Flask, redirect, url_for, render_template, request, Response
 from sympy.parsing.sympy_parser import parse_expr
 from sympy.utilities.lambdify import lambdify, implemented_function
 import matplotlib.pyplot as plot
@@ -10,7 +12,8 @@ from sympy.utilities.lambdify import lambdify, implemented_function
 from sympy import *
 import numpy as np
 import matplotlib.patches as polyplot
-
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 def parse(expresion):
     return parse_expr(expresion)
@@ -22,20 +25,6 @@ app = Flask(__name__)
 @app.route("/")
 def index(name=None, methods=["GET", "POST"]):
     return render_template("index.html", name=name)
-
-
-@app.route("/plot")
-def graphic():
-    error = None
-    if request.method == 'POST':
-        if valid_login(request.form['username'],
-                       request.form['password']):
-            return log_the_user_in(request.form['username'])
-        else:
-            error = 'Invalid username/password'
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
-    return render_template('login.html', error=error)
 
 
 def f(xyz):
@@ -57,11 +46,11 @@ def integral_plot(f, a, b, N):
     x = np.linspace(a, b, num=N)
     y = f(x)
     #fig = plot.subplots()
-    ax = plot.subplots()
-    ax.plot(x, y, 'ro', linewidth=3, color='pink')
-    plot.grid(True, linestyle=':')
-    plot.title(f'Integral')
-    plot.plot(legend=f'x:[{x}]')
+    # ax = plot.subplots()
+    # ax.plot(x, y, 'ro', linewidth=3, color='pink')
+    # plot.grid(True, linestyle=':')
+    # plot.title(f'Integral')
+    # plot.plot(legend=f'x:[{x}]')
 
     # Make the shaded region
     ix = np.linspace(a, b, num=N)
@@ -69,11 +58,11 @@ def integral_plot(f, a, b, N):
     verts = [(a, 0), *zip(ix, iy), (b, 0)]
     t = tuple(verts)
     p = Polygon(*t)
-    plot.Polygon
-    poly = polyplot.Polygon(t, facecolor='0.9', edgecolor='0.5')
-    ax.add_patch(poly)
-    plot.show()
-    return plot.savefig('static/photos/integral.png')
+    # plot.Polygon
+    # poly = polyplot.Polygon(t, facecolor='0.9', edgecolor='0.5')
+    # ax.add_patch(poly)
+    # plot.show()
+    pass
 
 
 @app.route('/trapz')
@@ -111,6 +100,20 @@ def butt():
 
     return render_template('graph.html')
 
+@app.route("/matplotlib.png")
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+def create_figure():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+    axis.plot(xs, ys)
+    return fig
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
